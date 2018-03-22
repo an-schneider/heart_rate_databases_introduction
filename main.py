6d5fecf5e64b93e92748de2b4dc8965075dca02c
Anthony Schneider <AnthonySchneider@Anthonys-MacBook-Pro-2.local>
2018-03-22 13:54:41 -0400

    add functioning add_heart_rate and print_user portions

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from pymodm import connect
+from flask import Flask, jsonify, request
 import models
 import datetime
 
+app = Flask(__name__)
 
-def add_heart_rate(email, heart_rate, time):
+connect("mongodb://localhost:27017/db")
+
+@app.route("/api/heart_rate", methods=["POST"])
+def add_heart_rate():
     """
     Appends a heart_rate measurement at a specified time to the user specified by
     email. It is assumed that the user specified by email exists already.
@@ -11,10 +16,17 @@ def add_heart_rate(email, heart_rate, time):
     :param heart_rate: number heart_rate measurement of the user
     :param time: the datetime of the heart_rate measurement
     """
+
+    r = request.get_json()
+    email = r["user_email"]
+    heart_rate = r["heart_rate"]
+    time = datetime.datetime.now()
+    print(r)
     user = models.User.objects.raw({"_id": email}).first()  # Get the first user where _id=email
     user.heart_rate.append(heart_rate)  # Append the heart_rate to the user's list of heart rates
     user.heart_rate_times.append(time)  # append the current time to the user's list of heart rate times
     user.save()  # save the user to the database
+    return jsonify("Data recorded")
 
 
 def create_user(email, age, heart_rate, time):
@@ -32,19 +44,25 @@ def create_user(email, age, heart_rate, time):
     u.save()  # save the user to the database
 
 
-def print_user(email):
+@app.route("/api/heart_rate/<user_email>", methods=["GET"])
+def print_user(user_email):
     """
     Prints the user with the specified email
     :param email: str email of the user of interest
     :return:
     """
+    email = user_email
     user = models.User.objects.raw({"_id": email}).first()  # Get the first user where _id=email
     print(user.email)
     print(user.heart_rate)
     print(user.heart_rate_times)
+    return jsonify("Success")
+
+
+
+
 
 if __name__ == "__main__":
-    connect("mongodb://localhost:27017/heart_rate_app")  # open up connection to db
     create_user(email="suyash@suyashkumar.com", age=24, heart_rate=60, time=datetime.datetime.now())  # we should only do this once, otherwise will overwrite existing user
     add_heart_rate("suyash@suyashkumar.com", 60, datetime.datetime.now())
     print_user("suyash@suyashkumar.com")
