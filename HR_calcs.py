57702eeb87bd04186f5dff77c7e114c51dec9c22
Anthony Schneider <AnthonySchneider@Anthonys-MacBook-Pro-2.local>
2018-03-23 00:45:22 -0400

    add tachycardia checking

--- HR_calcs.py
+++ HR_calcs.py
@@ -17,5 +17,31 @@ def find_cutoff_index(time_input, heart_rate_times):
     return cutoff_index
 
 
+def check_tachycardia(age, avg_hr):
+    if 0 <= age <= 1 and avg_hr > 159:
+        tach = 1
+    elif 1 < age <= 2 and avg_hr > 151:
+        tach = 1
+    elif 2 < age <= 4 and avg_hr > 137:
+        tach = 1
+    elif 4 < age <= 7 and avg_hr > 133:
+        tach = 1
+    elif 7 < age <= 11 and avg_hr > 130:
+        tach = 1
+    elif 11 < age <= 15 and avg_hr > 119:
+        tach = 1
+    elif age > 15 and avg_hr > 100:
+        tach = 1
+    else:
+        tach = 0
+    return tach
+
+
+test=check_tachycardia(3, 135)
+print(test)
+
+
+
+
 
 
